7bd98b8d4eed397650bc47281e3d7caca0566634
CarltonSam <carltonscharles@gmail.com>
2024-03-10 16:39:59 +0530

    added table structures

--- routers/models.py
+++ routers/models.py
@@ -15,3 +15,37 @@ class UserDetails(Base):
     email = Column(String,primary_key=True,index=True)
     hashed_password = Column(String)
 
+class CustomerDetails(Base):
+    __tablename__ = "customers"
+
+    customer_id = Column(String,primary_key=True,index=True)
+    name = Column(String)
+    address = Column(String)
+    phone_no = Column(Integer)
+    whatsapp_no = Column(Integer)
+
+class ItemDetails(Base):
+    __tablename__ = "itemprice"
+
+    item_id = Column(String,primary_key=True,index=True)
+    washing_price = Column(Integer)
+    ironing_price = Column(Integer)
+    washing_ironing_price = Column(Integer,default = washing_price+ironing_price)
+
+class OrderDetails(Base):
+    __tablename__ = "orders"
+
+    order_id = Column(String,primary_key=True,index=True)
+    date = Column(Date)
+    customer_id = Column(String)
+    cust_name = Column(String)
+    total_items = Column(Integer)
+    status = Column(String)
+    ready_date = Column(Date)
+    delivery_date = Column(Date)
+    cash = Column(Integer)
+    bank = Column(Integer)
+    advance_paid = Column(Integer)
+    due = Column(Integer)
+
+    
